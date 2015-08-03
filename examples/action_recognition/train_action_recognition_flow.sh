7377a6d14406b8ea656257561cf996e026adae72
yjxiong <xy012@ie.cuhk.edu.hk>
2015-08-03 11:30:17 +0800

    releasing UCF101 model and configurations.

---
+++ examples/action_recognition/train_action_recognition_flow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+GOOGLE_LOG_DIR=models/action_recognition/log \
+    mpirun -np 4 \
+    cmake_build/install/bin/caffe train \
+    --solver=models/action_recognition/vgg_16_flow_solver.prototxt
+
