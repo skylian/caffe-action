d30911fbde579d47db183f11fd9f4e04518a0e05
skylian <lianxiaochen@gmail.com>
2015-09-01 23:15:21 -0700

    spatial product layer

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -509,6 +509,47 @@ class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
 #endif
 
 /**
+ * @brief bottom[0] * bottom[1] across channels
+ *       bottom[0] dim: NxCxHxW
+ *       bottom[1] dim: Nx1xHxW
+ *       top[0]    dim: NxCxHxW where each channel is the spatial product between
+ *                      C-th channel of bottom[0] and bottom[1]
+ */
+template <typename Dtype>
+class SpatialProductLayer : public Layer<Dtype> {
+ public:
+  explicit SpatialProductLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline const char* type() const { return "SpatialProduct"; }
+
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int num_;
+  int channels_;
+  int height_;
+  int width_;
+
+  Blob<Dtype> multiplier_;  // dot multiplier for backward computation of params
+  Blob<Dtype> backward_buff_;  // temporary buffer for backward computation
+};
+
+/**
  * @brief Does spatial pyramid pooling on the input image
  *        by taking the max, average, etc. within regions
  *        so that the result vector of different sized
